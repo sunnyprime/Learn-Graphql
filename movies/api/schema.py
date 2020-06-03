342c7ee168b032b06d7a13c00e0f8653c457b1d6
unknown <kr.vivek.kr@gmail.com>
2020-06-03 12:29:18 +0530

    Name

--- movies/api/schema.py
+++ movies/api/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Movie
+from .models import Movie,Director
 
 class MovieType(DjangoObjectType):
     class Meta:
@@ -8,13 +8,20 @@ class MovieType(DjangoObjectType):
 
     movie_age = graphene.String()
 
-    def resolve_movie_aged(self, info):
-        return "Old movie" if self.year < 2000 else "New movie"
+    def resolve_movie_age(self, info):
+        return "Old movie" if self.year < 2005 else "New movie"
+
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model = Director
 
 class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
+    all_directors = graphene.List(DirectorType)
     movie = graphene.Field(MovieType,id=graphene.Int(),title =graphene.String())
     
+    def resolve_all_directors(self, info,**kwargs):
+        return Director.objects.all()
 
     def resolve_all_movies(self, info,**kwargs):
         return Movie.objects.all()
